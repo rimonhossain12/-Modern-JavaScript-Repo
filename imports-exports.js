6af91c06fbbf1ed12d8e0694c99a05a1492d0290
rimonspi12 <rimonmpi@gmail.com>
2021-09-14 12:59:02 +0600

    external file is added

---
+++ imports-exports.js
@@ -0,0 +1,8 @@
+//  imports/exports
+
+// import { pi as varA, a as varB} from './external.js';
+// console.log(varA, varB);
+
+import * as test from './external.js'; //  name import 
+console.log(test);
+// default import and exports;
